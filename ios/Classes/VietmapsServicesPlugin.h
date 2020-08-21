e0a0335065aae8d57d8c1672ad2e8243841f1bdd
AIC dev <nguyenphu2810.2@gmail.com>
2020-08-21 17:11:50 +0700

    update ios code

--- ios/Classes/VietmapsServicesPlugin.h
+++ ios/Classes/VietmapsServicesPlugin.h
@@ -1,4 +1,35 @@
 #import <Flutter/Flutter.h>
+@import MapboxGeocoder;
+
+@interface MBRectangularRegion : CLRegion
+@property (nonatomic) CLLocationCoordinate2D southWest;
+@property (nonatomic) CLLocationCoordinate2D northEast;
+
+- (nonnull instancetype)initWithSouthWest:(CLLocationCoordinate2D)southWest northEast:(CLLocationCoordinate2D)northEast;
+@end
+
+@interface MBGeocodeOptions : NSObject
+@property (nonatomic, strong) CLLocation * _Nullable focalLocation;
+@property (nonatomic, strong) MBRectangularRegion * _Nullable allowedRegion;
+@end
+
+@interface MBReverseGeocodeOptions : MBGeocodeOptions
+- (nonnull instancetype)initWithCoordinate:(CLLocationCoordinate2D)coordinate;
+@end
+
+@interface MBForwardGeocodeOptions : MBGeocodeOptions
+- (nonnull instancetype)initWithQuery:(NSString * _Nonnull)query;
+@end
+
+
+@interface MBGeocoder : NSObject
+- (nonnull instancetype)initWithAccessToken:(NSString * _Nullable)accessToken;
+
+- (NSURLSessionDataTask * _Nonnull)getInfoLocation:(MBReverseGeocodeOptions * _Nonnull)options completionHandler:(void (^ _Nonnull)(NSDictionary<NSString *, id> * _Nullable, NSError * _Nullable))completionHandler;
+
+- (NSURLSessionDataTask * _Nonnull)geocodeWithOptions:(MBGeocodeOptions * _Nonnull)options jsonCompletionHandler:(void (^ _Nonnull)(NSDictionary<NSString *, id> * _Nullable, NSError * _Nullable))completionHandler;
+
+@end
 
 @interface VietmapsServicesPlugin : NSObject<FlutterPlugin>
 @end
--- ios/Classes/VietmapsServicesPlugin.m
+++ ios/Classes/VietmapsServicesPlugin.m
@@ -1,6 +1,13 @@
 #import "VietmapsServicesPlugin.h"
 
+@interface VietmapsServicesPlugin()
+
+@property(nonatomic, strong) MBGeocoder *geocoder;
+
+@end
+
 @implementation VietmapsServicesPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"vietmaps_services"
@@ -8,13 +15,94 @@
   VietmapsServicesPlugin* instance = [[VietmapsServicesPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
-
+- (void) checkCreateServiceWithToken:(NSString *) token {
+    if (_geocoder == nil) {
+        _geocoder = [[MBGeocoder alloc] initWithAccessToken:token];
+    }
+}
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
+  }
+  else if ([@"getLocationInfo" isEqualToString:call.method]) {
+      NSDictionary *params = call.arguments;
+      if (params && params[@"access_token"] && params[@"query"]) {
+          NSString *token = params[@"access_token"];
+          [self checkCreateServiceWithToken:token];
+          NSArray *latLong = params[@"query"];
+          NSNumber *latNb = latLong[0];
+          NSNumber *longNb = latLong[1];
+          CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latNb.doubleValue, longNb.doubleValue);
+          MBReverseGeocodeOptions *reverseOption = [[MBReverseGeocodeOptions alloc] initWithCoordinate:coordinate];
+          [_geocoder getInfoLocation:reverseOption completionHandler:^(NSDictionary<NSString *,id> * _Nullable response, NSError * _Nullable error) {
+              if (error) {
+                  result(nil);
+              }
+              else {
+                  result([self convertDicToString:response]);
+              }
+          }];
+      }
+      else {
+          result(nil);
+      }
+  }
+  else if ([@"searchPlace" isEqualToString:call.method]) {
+      NSDictionary *params = call.arguments;
+      if (params && params[@"access_token"]) {
+          NSString *token = params[@"access_token"];
+          [self checkCreateServiceWithToken:token];
+          
+          NSString *query = params[@"query"];
+          MBForwardGeocodeOptions *forwardOption = [[MBForwardGeocodeOptions alloc] initWithQuery:query];
+          
+          NSArray *boundingBox = params[@"bounding_box"];
+          if (boundingBox) {
+              CLLocationCoordinate2D southWestCoor;
+              CLLocationCoordinate2D northEastCoor;
+              {
+                  NSArray *southWest = boundingBox[0];
+                  NSNumber *south = southWest[0];
+                  NSNumber *west = southWest[1];
+                  southWestCoor = CLLocationCoordinate2DMake(south.doubleValue, west.doubleValue);
+              }
+              {
+                  NSArray *northEast = boundingBox[1];
+                  NSNumber *north = northEast[0];
+                  NSNumber *east = northEast[1];
+                  northEastCoor = CLLocationCoordinate2DMake(north.doubleValue, east.doubleValue);
+              }
+              MBRectangularRegion *rectRegion = [[MBRectangularRegion alloc] initWithSouthWest:southWestCoor northEast:northEastCoor];
+              forwardOption.allowedRegion = rectRegion;
+          }
+          NSArray *proximity = params[@"proximity"];
+          if (proximity) {
+              NSNumber *latNb = proximity[0];
+              NSNumber *longNb = proximity[1];
+              CLLocation *focalLocation = [[CLLocation alloc] initWithLatitude:latNb.doubleValue longitude:longNb.doubleValue];
+              forwardOption.focalLocation = focalLocation;
+          }
+          [_geocoder geocodeWithOptions:forwardOption jsonCompletionHandler:^(NSDictionary<NSString *,id> * _Nullable response, NSError * _Nullable error) {
+              if (error) {
+                  result(nil);
+              }
+              else {
+                  result([self convertDicToString:response]);
+              }
+          }];
+      }
+      else {
+          result(nil);
+      }
+  }
+  else {
     result(FlutterMethodNotImplemented);
   }
 }
-
+- (NSString *) convertDicToString:(NSDictionary *) dic {
+    NSError * err;
+    NSData * jsonData = [NSJSONSerialization dataWithJSONObject:dic options:0 error:&err];
+    NSString * myString = [[NSString alloc] initWithData:jsonData   encoding:NSUTF8StringEncoding];
+    return myString;
+}
 @end
